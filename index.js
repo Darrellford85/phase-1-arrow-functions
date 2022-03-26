cdeafa74b2014011e03146afefedddb43f8835dd
Darrell Ford <Darrellford85@gmail.com>
2022-03-26 06:36:04 -0700

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// Complete the coding //
+
+// function expression called divide//
+const divide = (pmeter1, pmeter2) => 2000 / 100;
+
+//arrow function called square//
+
+const square = (x) => x**x;
+let x = 4
+
+//has an arrow function called add//
+const add = (d,g) => d+g;
+let d = 3
+let g = 4
